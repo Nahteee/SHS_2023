09e4d71a811c4c4ccb65c24e232546e35de8347c
hansonyiPP <hansonyip68@gmail.com>
2023-05-09 12:14:26 +0800

    new

--- src/main/java/SHS/UI/Java/Lab/Welcome.java
+++
@@ -1,9 +0,0 @@
-public class Welcome {
-
-/**
-       @param args the command line arguments
-*/
-public static void main(String[] args) {
-      System.out.println("My first program.");
-}
-}
--- src/main/java/SHS/UI/Programming For Data Analysis/19042023.R
+++
@@ -1,7 +0,0 @@
-# DATA MANIPULATION AND TRANSFORMATION
-
-install.packages("dplyr")
-library(tidyverse)
-
-# Create one csv file
-
